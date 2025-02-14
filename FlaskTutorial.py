320833e343279264047837f76822a8996682e6db
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-02-14 10:09:37 -0500

    Update FlaskTutorial.py

--- FlaskTutorial.py
+++ FlaskTutorial.py
@@ -1,10 +1,20 @@
-from flask import Flask, redirect,url_for,render_template
+from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 
-def home():
-    return render_template("personinfo.html")
+def main():
+    if request.method == 'GET':
+        return render_template('personinfo.html')
+    else:
+        GetInfo()
+        return render_template('personinfo.html')
+    
+def GetInfo():
+    name=request.form.get('txtname')
+    email=request.form.get('txtemail')
+    print("Hello " + name + ". Thank You for Your email " + email)
+    return "Hello " + name + ". Thank You for Your email " + email
 if __name__ == "__main__":
     app.run()
